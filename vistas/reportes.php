ddacdc0628ae773a6d39ea86dd6076649666e9c1
Diego Andres Benitez <47540258+andres-1994@users.noreply.github.com>
2021-06-08 19:57:52 -0400

    reportes version1

--- vistas/reportes.php
+++ vistas/reportes.php
@@ -1,5 +1,5 @@
 <?php
-	include('controladores/conexion.php');
+	include('../controladores/conexion.php');
 
 	session_start();
 
@@ -22,7 +22,7 @@
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.6/css/responsive.dataTables.min.css">
 	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;1,100&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="css/vista_menu.css">
+	<link rel="stylesheet" href="../css/vista_menu.css">
 	<title>Reportes Completos</title>
 	<style>
 		table>tbody>tr>td{
@@ -89,30 +89,24 @@
 <body>	
 	<div class="col-md-12">
 		<p id="alert" class="alert">Error de Conexion: Sin Internet!</p>
-		<?php include("vistas/vista_menu.php");?>
 		<h1 class="text-center mt-1">Teisa Comerciales & Soporte CAC</h1>
 		<div class="card">
 			<div class="card-header">
-				<?php include("controladores/scripts.php") ?>
+				Reportes 
 			</div>
 			<div class="card-body">
-				<form action="controladores/eliminar_marcado.php" method="POST">
-					<div>
-						<button type="button" class="mt-1 mb-1 btn btn-primary"><a class="text-white" href="form_insert_personal"><i class="fas fa-plus"></i> Agregar Nuevo</a></button>
-						<button type="submit" name="eliminar-marcado" class="mt-1 mb-1 btn btn-danger" onclick="reload()"><a class="text-white"><i class="fas fa-trash-alt"></i> Eliminar Marcado</a></button>
-					</div>
+				<form action="../controladores/eliminar_marcado.php" method="POST">
 					<table id="mi-tabla" class="table table-striped table-bordered table-hover display nowrap" cellspacing="0" style="width:100%">
 						<thead>
 							<tr>
-								<th></th>
-								<th></th>
 								<th>Código</th>
 								<th>CI/DNI</th>
 								<th>Nombre</th>
 								<th>Apellido</th>
 								<th>Teléfono</th>
 								<th>Dirección</th>
-								<th></th>
+								<th>Correo</th>
+								<th>Estado</th>
 							</tr>
 						</thead>
 						<tbody>
@@ -122,27 +116,14 @@
 							while($row = mysqli_fetch_assoc($resultado)){
 								?>
 								<tr>
-									<td></td>
-									<td>
-										<a href="vistas/datos_completos?id=<?php echo base64_encode($row['id']); ?>" title="Ver ficha completa" style="font-size: 25px;">
-											<i class="fas fa-plus-circle"></i>
-										</a>
-									</td>
 									<td><?php echo $row['codigo']; ?></td>
 									<td><?php echo number_format($row['ci'],0,'','.'); ?></td>
 									<td><?php echo $row['nombre']; ?></td>
 									<td><?php echo $row['apellido']; ?></td>
 									<td><?php echo $row['telefono']; ?></td>
 									<td><?php echo $row['direccion']; ?></td>
-									<td>
-										<a title='Editar Empleado' href="form_modifica_personal?id=<?php echo base64_encode($row['id']); ?>">
-											<i class="fas fa-edit"></i>
-										</a><br>
-										<a title='Eliminar Empleado' class="link-operacion" href="controladores/eliminar_personal?id=<?php echo $row['id']; ?>">
-											<i class="fas fa-trash-alt"></i>
-										</a><br>
-										<input type="checkbox" name="eliminar[]" value="<?php echo $row['id'];?>">	 
-									</td>
+									<td><?php echo $row['correo']; ?></td>
+									<td><?php echo $row['estado']; ?></td>
 								</tr>
 							<?php  } mysqli_free_result($resultado); ?>
 						</tbody>	
@@ -156,7 +137,6 @@
 	</div>		
 	<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 	<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
-	<script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 	<script src="https://cdn.datatables.net/responsive/2.2.6/js/dataTables.responsive.min.js"></script>
 	<script src="https://cdn.datatables.net/select/1.3.1/js/dataTables.select.min.js"></script>
@@ -166,6 +146,5 @@
 	<script src="https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.min.js"></script>
 	<script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.html5.min.js"></script>
 	<script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.print.min.js"></script>
-	<script src="javascript/scripts.js"></script>	
 </body>
 </html>
